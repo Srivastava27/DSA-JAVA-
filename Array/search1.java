5a33746810ef84bd99a5b9e84a8449c26e4c96e7
Srivastava27 <vanshikas2623@gmail.com>
2022-12-17 14:57:20 +0530

    Initial search

--- Array/search1.java
+++ Array/search1.java
@@ -3,7 +3,7 @@ package Array;
 class ElementSearch{
     public static void main(String[] args){
         int arr[]={2,4,6,88,100};
-         int res=0;
+         int res=-1;  //-1 Means element is not there  
          int key=88;
         for(int i=0;i<arr.length;i++){
             if(arr[i]==key){
@@ -11,7 +11,7 @@ class ElementSearch{
                 break;
             }
         }
-        if(res==0){
+        if(res==-1){
             System.out.println("Element does not found");
         }else{
             System.out.println("Element found at index: "+res);
