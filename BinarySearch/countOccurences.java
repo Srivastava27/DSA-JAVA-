0206face34f6eaac60bb6c008c670ae2a80cf3aa
Srivastava27 <vanshikas2623@gmail.com>
2023-01-11 00:43:51 +0530

    count occurences in a sorted array

--- BinarySearch/countOccurences.java
+++ BinarySearch/countOccurences.java
@@ -3,13 +3,47 @@
 package BinarySearch;
 
 class Count{
-    
-    public static int res(int arr[],int key)
-    
+//first occurence     
+    public static int first(int arr[],int key,int start,int end){
+    int ans=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key){
+                ans=mid;
+                end=mid-1;
+            }
+            if(key > arr[mid]){
+                start=mid+1; 
+            }else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public static int last(int arr[],int key, int start,int end){
+        int ans=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key){
+                ans=mid;
+                end=mid+1;
+            }
+            if(key < arr[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;    
+            }
+        }
+        return ans;
+    }
     
     public static void main(String[] args) {
-        int arr[]={2,3,3,3,4,5,6,7,7,7};
+        int arr[]={2,3,3,3,3,3,4,5,6,7,7,7};
         int key=3;
-        
+        int start=0;
+        int end=arr.length;
+        int temp=(last(arr, key, start, end)-first(arr, key, start, end))+1;
+        System.out.println(temp);
     }
 }
--- BinarySearch/rotatedArray.java
+++ BinarySearch/rotatedArray.java
@@ -1,7 +1,43 @@
 package BinarySearch;
 
 class SearchInRotatedSortedArray{
+    
+    public static int ans(int arr[],int key,int start,int end,int temp){
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==key)return mid;
+            if(arr[start]<arr[mid]){
+                if(key<arr[mid] && key<arr[start]){
+                    end=mid-1;
+                }else{
+                    if(key>arr[mid] && key>arr[start]){
+                        start=mid+1;
+                    }
+                }
+            }else{
+                if(arr[end]<arr[mid]){
+                    if(key>arr[mid] && key>arr[end]){
+                        start=mid=1;
+                    }else{
+                        if(key<arr[mid] && key<arr[end]){
+                            end=mid-1;
+                        }
+                    }
+                }
+            }
+
+        }
+        return -1;
+    }
+    
+    
     public static void main(String[] args) {
-        
+        int arr[]={20,30,40,50,60,5,10};
+        int key=60;
+        int start=0;
+        int end=arr.length-1;
+        int temp=0;
+        int res=ans(arr, key, start, end, temp);
+        System.out.println(res);
     }
 }
