c95b708bfe163c75dd8fe67779a098ab88675dc8
Srivastava27 <vanshikas2623@gmail.com>
2023-01-13 21:45:52 +0530

    median

--- BinarySearch/Median.java
+++ BinarySearch/Median.java
@@ -3,7 +3,9 @@ package BinarySearch;
 class MedianInInfiniteArray{
     public static void main(String[] args) {
         int arr[]={2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,26,27,28,29,30,31};
-        int median=0;
+        int med=0;
+        for()
+
         
     }
 }
