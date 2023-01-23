015a3443ffbdff229f7c3b3c4b4cf4b1a8c8b259
Srivastava27 <vanshikas2623@gmail.com>
2023-01-23 12:01:37 +0530

    maximum subarray

---
+++ Array/maximumSubarray.java
@@ -0,0 +1,26 @@
+package Array;
+
+class maximum_subarray{
+
+    public static int maxSubArray(int[] arr) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[]={-2,-1,-3,4,-1,2,1,-5,4};
+        int res=maxSubArray(arr);
+        System.out.println(res);
+    }
+}
