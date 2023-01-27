3a0e953f390a8a2068b697eecfc09c05d1c0df2b
Srivastava27 <vanshikas2623@gmail.com>
2023-01-28 00:57:31 +0530

    contains Duplicate

--- DSAsheetApnaCollege/ContainsDuplicate.java
+++ DSAsheetApnaCollege/ContainsDuplicate.java
@@ -1,8 +1,9 @@
-//--------------------without hashset-----------------------------//
+//--------------------without hashing-----------------------------//
 
 package DSAsheetApnaCollege;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
 
@@ -19,6 +20,33 @@ public class ContainsDuplicate {
     public static void main(String[] args) {
         int arr[]={1,2,3,1};
         boolean res=Duplicate(arr);
+        System.out.println(res);
+    }
+}
+
+
+//-----------------------with hashing----------------------------------//
+
+class Duplicate{
+
+    public static boolean Dup(int arr[]){
+            HashSet<Integer> hashSet=new HashSet();
+            for(int i=0;i<arr.length;i++){
+                if(hashSet.contains(arr[i])){
+                    return true;
+                }else{
+                    hashSet.add(arr[i]);
+                }
+            }
+            return false;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,1};
+        boolean res=Dup(arr);
         System.out.println(res);
     }
 }
