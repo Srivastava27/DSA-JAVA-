26e70d636102250a8067b0902e1dc8ce20871e00
Srivastava27 <vanshikas2623@gmail.com>
2023-02-04 16:40:34 +0530

    parenthesis are valid or not

---
+++ Stack/Parenthesis.java
@@ -0,0 +1,54 @@
+package Stack;
+
+import java.util.ArrayDeque;
+
+class Parenthesis {
+
+    static boolean isValid(String str){
+        ArrayDeque<Character> stack=new ArrayDeque<>();
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            if(curr=='{' || curr=='(' ||curr=='['){
+                stack.push(curr);
+                continue;
+            }
+
+            char head='a';
+            switch(curr){
+                case ')':
+                head=stack.pop();
+                if(head!='('){
+                    return false;
+                    
+                }
+                break;
+
+                case '}':
+                head=stack.pop();
+                if(head!='{'){
+                    return false;
+                    
+                }
+                break;
+
+                case ']':
+                head=stack.pop();
+                if(head!='['){
+                    return false;
+                    
+                }
+                break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+       String str="{a[(a-b)(i}";
+    //    String str2=" ";
+
+       System.out.println(isValid(str));
+
+
+    }
+}
