546dc73a09fc0a5af4d15d74aa3861240f4796fd
Srivastava27 <vanshikas2623@gmail.com>
2023-02-04 16:04:00 +0530

    Implementation using LinkedList

---
+++ Stack/ImplementationUsingLinkedList.java
@@ -0,0 +1,49 @@
+package Stack;
+
+import javax.swing.tree.ExpandVetoException;
+
+public class ImplementationUsingLinkedList {
+
+    Node head;
+    int size=0;
+
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+        }
+    }
+
+    void push(int element){
+        Node newNode=new Node(element);
+        newNode.next=head;
+        head=newNode;
+        size++;
+
+    }
+
+    boolean isEmpty(){
+        return size==0;
+    }
+
+    int pop()throws Exception{
+        if(isEmpty())throw new Exception("Stack is empty");
+        int ans=head.data;
+        head=head.next;
+        size--;
+        return ans;
+    }
+
+    int peak()throws Exception{
+        if(isEmpty())throw new Exception("Stack is empty");
+        return head.data;
+    }
+
+
+    int size(){
+        return size;
+    }
+
+}
