2a310b76c4bd08b57c4efb486240f0388170125e
Srivastava27 <vanshikas2623@gmail.com>
2023-02-10 00:48:57 +0530

    string concatenation and string charAt Function

--- Strings/Concatenation.java
+++ Strings/Concatenation.java
@@ -4,8 +4,14 @@ public class Concatenation {
     public static void main(String[] args) {
         //concatenation
         String firstname="vanshika";
-        String Lastname=" srivastava";
-        String fullname=firstname+Lastname;
+        String Lastname="srivastava";
+        String fullname=firstname+" "+Lastname;
         System.out.println(fullname);
+        System.out.println(fullname.length());
+
+        //charAt           it will each character one by one
+        for(int i=0;i<fullname.length();i++){
+            System.out.println(fullname.charAt(i));
+        }
     }
 }
