d2c15fb6bc0ecb652f396d5f0dbfdac8986cfc76
Srivastava27 <vanshikas2623@gmail.com>
2023-02-10 00:40:13 +0530

    concatenation of strings

---
+++ Strings/Concatenation.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        //concatenation
+        String firstname="vanshika";
+        String Lastname=" srivastava";
+        String fullname=firstname+Lastname;
+        System.out.println(fullname);
+    }
+}
