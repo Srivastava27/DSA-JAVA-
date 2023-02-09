7af0493f0f026b9cb04edf54508d3daaaf23b8a6
Srivastava27 <vanshikas2623@gmail.com>
2023-02-10 01:26:53 +0530

    Reverse a string

---
+++ Strings/ReverseString.java
@@ -0,0 +1,21 @@
+package Strings;
+
+//Strings are immutable which means once the data entered cannot be changed
+
+class ReverseString {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("hello");
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back=sb.length()-1-i;
+
+            char frontChar=sb.charAt(front);
+            char backChar=sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println(sb);
+    }    
+}
