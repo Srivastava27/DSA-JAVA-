c71d464ba0d8d72a92c371df0a0401b6180e6a30
Srivastava27 <vanshikas2623@gmail.com>
2023-02-10 01:01:56 +0530

    Substring

---
+++ Strings/substring.java
@@ -0,0 +1,12 @@
+package Strings;
+
+
+class substring {
+    public static void main(String[] args) {
+        String sentence="TonyStark";
+        String name=sentence.substring(4,6);
+        System.out.println(name);
+
+        // substring will only print those characters whose begining and ending index are given
+    }
+}
